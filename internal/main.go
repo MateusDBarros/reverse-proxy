ea64bca3b4d505114ca1cbb0c5c05b8c6c8e954e
Mateus Barros <MateusBarros@ibm.com>
2026-03-03 16:03:07 -0300

    updating bussines logic to use 'rewrite'

--- internal/main.go
+++ internal/main.go
@@ -1,60 +1,39 @@
 package main
 
 import (
-	"context"
 	"log"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
-	"time"
+	// any other imports you need
 )
 
 func main() {
 
 	pool := ServerPool{}
 
-	addresses := [3]string{"http://localhost:8081", "http://localhost:8082", "http://localhost:8083"}
+	addresses := []string{"http://localhost:8080", "http://localhost:8081", "http://localhost:8082", "http://localhost:8083"}
 
-	for _, address := range addresses {
-		targetURL, _ := url.Parse(address)
+	for _, addr := range addresses {
 
-		proxy := httputil.NewSingleHostReverseProxy(targetURL)
-
-		var b *Backend
-
-		proxy.ErrorHandler = func(w http.ResponseWriter, r *http.Request, e error) {
-			log.Printf("Proxy Error: %v", e)
-
-			b.SetAlive(false)
-
-			attempts, _ := r.Context().Value(Attempts).(int)
-			if attempts == 0 {
-				attempts = 1
-			}
-
-			ctx := context.WithValue(r.Context(), Attempts, attempts+1)
-
-			pool.HttpHandler(w, r.WithContext(ctx))
+		parsedUrl, err := url.Parse(addr)
+		if err != nil {
+			log.Fatal(err)
 		}
 
-		b = &Backend{
-			url:   targetURL,
-			proxy: proxy,
-			alive: true,
-		}
-		pool.AddBackend(b)
-	}
+		proxy := &httputil.ReverseProxy{
+			Rewrite: func(pr *httputil.ProxyRequest) {
+				pr.SetURL(parsedUrl)
+				pr.SetXForwarded()
+			},
+			ErrorHandler: func(w http.ResponseWriter, r *http.Request, err error) {
 
-	go func() {
-		for {
-			log.Println("Performing health check")
-			pool.HealthCheck()
-			time.Sleep(10 * time.Second)
+				log.Printf("Proxy error: %v", err)
+			},
 		}
-	}()
 
-	err := http.ListenAndServe(":9420", http.HandlerFunc(pool.HttpHandler))
-	if err != nil {
-		log.Fatalf("Error starting server: %s", err)
+		backend := &Backend{url: parsedUrl, alive: true, proxy: proxy}
+		pool.AddBackend(backend)
+
 	}
 }
